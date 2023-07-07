2d5960bfc49a70d8d2b5228ff992bb038c6be87c
Noviria Syifaun Nafsi <133358245+nafsi20@users.noreply.github.com>
2023-07-07 12:51:50 +0200

    please no more errors!!

--- with new variables.R
+++ with new variables.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(decisionSupport)
+library(ggplot2)
 
 read.csv("new_variable_estimates.csv", sep=";")
 
@@ -18,69 +19,84 @@ crop_rotation_decision <- function(){
   #Estimate the cost of rice farm in a normal season
   rice_cost <- sum(rice_land_rental_cost, rice_seeds_cost, rice_fertilizer_cost,
                    rice_pesticide_cost, rice_machinery_cost, rice_harvesting_cost)
-    
-    
+  
   #Estimate the cost of soybean farm in a normal season
   soybean_cost <- sum(soybean_land_rental_cost, soybean_seeds_cost, soybean_fertilizer_cost,
                       soybean_pesticide_cost, soybean_machinery_cost, soybean_harvesting_cost)
-  
   
   #Estimate the cost of chili farm in a normal season
   chili_cost <- sum(chili_land_rental_cost, chili_seeds_cost, chili_fertilizer_cost,
                     chili_pesticide_cost, chili_machinery_cost, chili_harvesting_cost)
   
+  # Estimate the profit
+  rice_profit <- vv(rice_income - rice_cost,n=n_year,var_CV=100)
+  soybean_profit <- vv(soybean_income - soybean_cost,n=n_year,var_CV=100)
+  chili_profit <- vv(chili_income - chili_cost,n=n_year,var_CV=100)
   
+  # Final
+  crop_rotation_result = rice_profit + soybean_profit + chili_profit
+  rice_cultivation_result = rice_profit*3
   
-  # Estimate the final results from the model
-  final_result_rice <- rice_income - rice_cost
-  
-  final_result_soybean <- soybean_income - soybean_cost
-  
-  final_result_chili <- chili_income - chili_cost
-  
-  
-  # Total benefits from crop production (agricultural development and riparian zone) ####
-  crop_rotation_all <-
-    intervention_fruit_benefits +
-    intervention_vegetable_benefits +
-    intervention_rainfed_crop_benefits
-  
+  # NPV
+  NPV_total <- discount(crop_rotation_result, discount_rate, calculate_NPV = TRUE)
+  NPV_rice <- discount(rice_cultivation_result, discount_rate, calculate_NPV = TRUE)
+  NPV_total_income <- discount(rice_income + soybean_income + chili_income, discount_rate, calculate_NPV = TRUE)
+  NPV_rice_income <- discount(rice_income, discount_rate, calculate_NPV = TRUE)
+  NPV_total_cost <- discount(rice_cost + soybean_cost + chili_cost, discount_rate, calculate_NPV = TRUE)
+  NPV_rice_cost <- discount(rice_cost, discount_rate, calculate_NPV = TRUE)
   
   # Generate the list of outputs from the Monte Carlo simulation
-  return(list(final_result = final_result_rice + final_result_soybean + final_result_chili))
+  return(list(NPV_total=NPV_total, NPV_rice=NPV_rice,
+              NPV_total_income=NPV_total_income,NPV_rice_income=NPV_rice_income,
+              NPV_total_cost=NPV_total_cost,NPV_rice_cost=NPV_rice_cost))
 }
-
-
 
 # Run the Monte Carlo simulation using the model function
 
 input_estimates <-read.csv("new_variable_estimates.csv", sep=";")
 
+str(input_estimates)
+
 crop_rotation_mc_simulation <- mcSimulation(estimate = as.estimate(input_estimates),
-                                      model_function = crop_rotation_decision,
-                                      numberOfModelRuns = 500,
-                                      functionSyntax = "plainNames")
+                                            model_function = crop_rotation_decision,
+                                            numberOfModelRuns = 500,
+                                            functionSyntax = "plainNames")
 
-input_estimates
-
-
-mcSimulation_results <- decisionSupport::mcSimulation(
-  estimate = decisionSupport::estimate_read_csv("new_variable_estimates.csv", sep=";"),
-  model_function = crop_rotation_decision,
-  numberOfModelRuns = 500,
-  functionSyntax = "plainNames"
-)
+crop_rotation_mc_simulation
 
 
+# plot NPV distribution analysis
+
+decisionSupport::plot_distributions(mcSimulation_object = crop_rotation_mc_simulation, 
+                                    vars = c("NPV_total","NPV_rice"),
+                                    method = 'smooth_simple_overlay')
+
+decisionSupport::plot_distributions(mcSimulation_object = crop_rotation_mc_simulation, 
+                                    vars = c("NPV_total_income","NPV_rice_income"),
+                                    method = 'smooth_simple_overlay')
+
+decisionSupport::plot_distributions(mcSimulation_object = crop_rotation_mc_simulation, 
+                                    vars = c("NPV_total_cost","NPV_rice_cost"),
+                                    method = 'smooth_simple_overlay')
+
+# cashflow analysis
+
+plot_cashflow(mcSimulation_object = crop_rotation_mc_simulation, cashflow_var_name = "crop_rotation_result")
+plot_cashflow(mcSimulation_object = crop_rotation_mc_simulation, cashflow_var_name = "rice_cultivation_result")
 
 
-library(ggplot2)
+# Projection to Latent Structures (PLS) analysis
 
-plot_distributions(mcSimulation_object = crop_rotation_mc_simulation,
-                   vars = "final_result",
-                   method = "boxplot_density",
-                   old_names = "final_result",
-                   new_names = "Outcome distribution for profits")
+
+pls_result <- plsr.mcSimulation(object = crop_rotation_mc_simulation,
+                                resultName = "NPV_total", ncomp = 1)
+plot_pls(pls_result, threshold = 0)
+
+pls_result_rice <- plsr.mcSimulation(object = crop_rotation_mc_simulation,
+                                     resultName = "NPV_rice", ncomp = 1)
+plot_pls(pls_result_rice, threshold = 0)
+
+
 
 
 make_variables <- function(est,n=1)
@@ -91,7 +107,3 @@ for(i in colnames(x)) assign(i,
 
 
 make_variables(as.estimate(input_estimates))
-
-
-
-
