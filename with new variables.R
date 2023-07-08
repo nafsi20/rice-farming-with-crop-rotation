21214739397fb5b9d2c1a6e7d6b68f217926d2dc
Noviria Syifaun Nafsi <noviriasyifaunnafsi@gmail.com>
2023-07-08 02:04:56 +0200

    fixed!

--- with new variables.R
+++ with new variables.R
@@ -33,26 +33,27 @@ crop_rotation_decision <- function(){
   soybean_profit <- vv(soybean_income - soybean_cost,n=n_year,var_CV=100)
   chili_profit <- vv(chili_income - chili_cost,n=n_year,var_CV=100)
   
-  # Final
+  # Final result
   crop_rotation_result = rice_profit + soybean_profit + chili_profit
   rice_cultivation_result = rice_profit*3
   
   # NPV
-  NPV_total <- discount(crop_rotation_result, discount_rate, calculate_NPV = TRUE)
+  NPV_crop_rotation <- discount(crop_rotation_result, discount_rate, calculate_NPV = TRUE)
   NPV_rice <- discount(rice_cultivation_result, discount_rate, calculate_NPV = TRUE)
-  NPV_total_income <- discount(rice_income + soybean_income + chili_income, discount_rate, calculate_NPV = TRUE)
+  NPV_crop_rotation_income <- discount(rice_income + soybean_income + chili_income, discount_rate, calculate_NPV = TRUE)
   NPV_rice_income <- discount(rice_income, discount_rate, calculate_NPV = TRUE)
-  NPV_total_cost <- discount(rice_cost + soybean_cost + chili_cost, discount_rate, calculate_NPV = TRUE)
+  NPV_crop_rotation_cost <- discount(rice_cost + soybean_cost + chili_cost, discount_rate, calculate_NPV = TRUE)
   NPV_rice_cost <- discount(rice_cost, discount_rate, calculate_NPV = TRUE)
   
   # Generate the list of outputs from the Monte Carlo simulation
-  return(list(NPV_total=NPV_total, NPV_rice=NPV_rice,
-              NPV_total_income=NPV_total_income,NPV_rice_income=NPV_rice_income,
-              NPV_total_cost=NPV_total_cost,NPV_rice_cost=NPV_rice_cost))
+  return(list(NPV_crop_rotation=NPV_crop_rotation, NPV_rice=NPV_rice,
+              NPV_crop_rotation_income=NPV_crop_rotation_income, NPV_rice_income=NPV_rice_income,
+              NPV_crop_rotation_cost=NPV_crop_rotation_cost, NPV_rice_cost=NPV_rice_cost,
+              crop_rotation_result=crop_rotation_result,
+              rice_cultivation_result=rice_cultivation_result))
 }
 
 # Run the Monte Carlo simulation using the model function
-
 input_estimates <-read.csv("new_variable_estimates.csv", sep=";")
 
 str(input_estimates)
@@ -62,38 +63,64 @@ crop_rotation_mc_simulation <- mcSimulation(estimate = as.estimate(input_estimat
                                             numberOfModelRuns = 500,
                                             functionSyntax = "plainNames")
 
-crop_rotation_mc_simulation
-
 
 # plot NPV distribution analysis
-
 decisionSupport::plot_distributions(mcSimulation_object = crop_rotation_mc_simulation, 
-                                    vars = c("NPV_total","NPV_rice"),
+                                    vars = c("NPV_crop_rotation","NPV_rice"),
+                                    method = 'smooth_simple_overlay')
+
+
+#just try, but we aren't using these
+decisionSupport::plot_distributions(mcSimulation_object = crop_rotation_mc_simulation, 
+                                    vars = c("NPV_crop_rotation_income","NPV_rice_income"),
                                     method = 'smooth_simple_overlay')
 
 decisionSupport::plot_distributions(mcSimulation_object = crop_rotation_mc_simulation, 
-                                    vars = c("NPV_total_income","NPV_rice_income"),
+                                    vars = c("NPV_crop_rotation_cost","NPV_rice_cost"),
                                     method = 'smooth_simple_overlay')
 
-decisionSupport::plot_distributions(mcSimulation_object = crop_rotation_mc_simulation, 
-                                    vars = c("NPV_total_cost","NPV_rice_cost"),
-                                    method = 'smooth_simple_overlay')
 
-# cashflow plot
+# cashflow analysis
 
+#with crop rotation
 plot_cashflow(mcSimulation_object = crop_rotation_mc_simulation, cashflow_var_name = "crop_rotation_result")
+
+#without crop rotation
 plot_cashflow(mcSimulation_object = crop_rotation_mc_simulation, cashflow_var_name = "rice_cultivation_result")
+
+
+
+# Vol analysis
+mcSimulation_table <- data.frame(crop_rotation_mc_simulation$x, crop_rotation_mc_simulation$y[1:3])
+
+evpi <- multi_EVPI(mc = mcSimulation_table, first_out_var = "NPV_crop_rotation")
+
+
+plot_evpi(evpi, decision_vars = "NPV_crop_rotation")
+plot_evpi(evpi, decision_vars = "NPV_rice")
+
 
 
 # Projection to Latent Structures (PLS) analysis
 
-
-pls_result <- plsr.mcSimulation(object = crop_rotation_mc_simulation,
-                                resultName = "NPV_total", ncomp = 1)
+#with crop rotation
+pls_result_crop_rotation <- plsr.mcSimulation(object = crop_rotation_mc_simulation,
+                                resultName = "NPV_crop_rotation", ncomp = 1)
 plot_pls(pls_result, threshold = 0)
 
+
+#without crop rotation
 pls_result_rice <- plsr.mcSimulation(object = crop_rotation_mc_simulation,
                                      resultName = "NPV_rice", ncomp = 1)
 plot_pls(pls_result_rice, threshold = 0)
+
+
+
+# the plots
+compound_figure(mcSimulation_object = crop_rotation_mc_simulation, 
+                input_table = input_estimates, plsrResults = pls_result_crop_rotation, 
+                EVPIresults = evpi, decision_var_name = "NPV_crop_rotation", 
+                cashflow_var_name = "crop_rotation_result", 
+                base_size = 7)
 
 
